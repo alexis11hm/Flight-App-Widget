1e9f04e1824233f8d28a0460272aafdf5d4bbe99
Alexis Hernandez Mondragon <ahermond@emeal.nttdata.com>
2024-06-19 11:04:55 -0600

    feature: pin shortcut added

--- app/src/main/java/com/alexis11hm/flightswidgetapp/ui/screens/HomeActivity.kt
+++ app/src/main/java/com/alexis11hm/flightswidgetapp/ui/screens/HomeActivity.kt
@@ -1,8 +1,14 @@
 package com.alexis11hm.flightswidgetapp.ui.screens
 
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -12,6 +18,7 @@ import com.alexis11hm.flightswidgetapp.data.model.Flight
 import com.alexis11hm.flightswidgetapp.databinding.ActivityHomeBinding
 import com.alexis11hm.flightswidgetapp.ui.registry.FlightRegistry
 import com.alexis11hm.flightswidgetapp.ui.viewmodels.FlightViewModel
+import com.alexis11hm.flightswidgetapp.ui.widgets.FlightWidget
 import kotlinx.coroutines.launch
 
 class HomeActivity : AppCompatActivity() {
@@ -32,6 +39,9 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun init(){
+        if (isVersionEqualsOrGreaterThanOreo()){
+            showPinShortcut()
+        }
         getData()
     }
 
@@ -66,6 +76,25 @@ class HomeActivity : AppCompatActivity() {
             binding.departureDateText.text = departureTime
             binding.arrivalDateText.text = arrivalTime
             binding.priceText.text = price
+        }
+    }
+
+    private fun isVersionEqualsOrGreaterThanOreo(): Boolean {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun showPinShortcut(){
+        val appWidgetManager = AppWidgetManager.getInstance(this)
+        val myProvider = ComponentName(this, FlightWidget::class.java)
+
+        if (appWidgetManager.isRequestPinAppWidgetSupported) {
+            val successCallback = PendingIntent.getBroadcast(
+                this,
+                0,
+                Intent(),
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+
+            appWidgetManager.requestPinAppWidget(myProvider, null, successCallback)
         }
     }
 }
